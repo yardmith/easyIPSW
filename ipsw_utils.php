a14725869520eff12a7cd5fe4f42bffb3a60817b
Adam <me@adamenglish.net>
2026-05-29 01:52:28 +0000

    Added functionality for decrypting and extracting DMGs, haven't implemented them in WebSockets though. Also some IMG container stuff which will be important later probably

--- ipsw_utils.php
+++ ipsw_utils.php
@@ -3,6 +3,87 @@
 use React\EventLoop\LoopInterface;
 
 require_once "db.php";
+require_once "constants.php";
+
+function identifyImg($path) {
+  $start = file_get_contents($path, length: 10);
+
+  if (str_starts_with($start, "8900")) {
+    return 2;
+  } elseif (str_starts_with($start, "3gmI")) {
+    return 3;
+  } elseif (str_starts_with($start, "0") && str_contains($start, "IM4P")) {
+    return 4;
+  } else {
+    return false;
+  }
+}
+
+function img2IsEncrypted($path) {
+  if (ord(file_get_contents($path, offset: 7, length: 1)) == 3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getKeysFromPath($path) {
+  global $db;
+
+  $pathParts = explode("/", $path);
+  $cacheIdx = array_search(CACHE_DIR, $pathParts);
+  if ($cacheIdx === false || count($pathParts) <= $cacheIdx + 1) {
+    return false;
+  }
+
+  $ipswId = $pathParts[$cacheIdx + 1];
+  $filename = pathinfo($path, PATHINFO_BASENAME);
+  if (!array_key_exists("keys", $db["ipsw"][$ipswId]) || !array_key_exists($filename, $db["ipsw"][$ipswId]["keys"])) {
+    return false;
+  }
+
+  return $db["ipsw"][$ipswId]["keys"][$filename];
+}
+
+function decryptImg($path) {
+  $type = identifyImg($path);
+
+  if ($type > 2) {
+    $keys = getKeysFromPath($path);
+    if (!$keys) return false;
+  }
+
+  switch ($type) {
+    case 2:
+      exec("bin/xpwntool $path $path");
+      break;
+    case 3:
+      exec("bin/xpwntool $path $path -k " . $keys["key"] . " -iv " . $keys["iv"]);
+      break;
+    case 4:
+      exec("bin/img4 -i $path -o $path -k " . $keys["iv"] . $keys["key"]);
+      break;
+    default:
+      return false;
+  }
+}
+
+function extractDmg($path) {
+  if (identifyImg($path) !== false) {
+    if (decryptImg($path) === false) return false;
+  } else {
+    $keys = getKeysFromPath($path);
+    if (!$keys) return false;
+    exec("bin/dmg extract $path $path -k " . $keys["key"]);
+  }
+
+  $dirname = pathinfo($path, PATHINFO_DIRNAME);
+  $oldList = scandir($dirname);
+  exec("bin/7zz x -o" . $dirname . " -y $path");
+  $dir = array_values(array_diff(scandir($dirname), $oldList))[0];
+  unlink($path);
+  rename($dirname . "/$dir", $path);
+}
 
 function getDirListing($path) {
   $listing = array_values(array_diff(scandir($path), [".", ".."]));
@@ -11,6 +92,9 @@ function getDirListing($path) {
 
   for ($i = 0; $i < count($listing); $i++) {
     $name = $listing[$i];
+
+    if (in_array($name, [".HFS+ Private Directory Data", ".Trashes", "[HFS+ Private Data]"])) continue;
+
     if (is_dir("$path/$name") && pathinfo($name, PATHINFO_EXTENSION) != "dmg") {
       array_push($dirs, $name);
     } else {
