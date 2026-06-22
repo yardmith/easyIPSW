b7864b2db3e4cb1a54383c1dc01476de706d7e94
Adam <me@adamenglish.net>
2026-06-22 05:19:22 +0000

    Fixed a bug where the ramdisk_update tag wouldn't get applied correctly

--- ipsw_utils.php
+++ ipsw_utils.php
@@ -376,7 +376,7 @@ function getDirListing($path, $includeTags = true) {
         if ($tag) $files[$filename]["tag"] = $tag;
       }
 
-      $tags[$updateManifest["RestoreRamDisk"]["Info"]["Path"]] = "ramdisk_update";
+      $files[$updateManifest["RestoreRamDisk"]["Info"]["Path"]]["tag"] = "ramdisk_update";
     } elseif (is_file("$cachePath/Restore.plist")) {
       // Fall back to Restore.plist, which even the oldest IPSWs have
       $restorePlist = (new CFPropertyList("$cachePath/Restore.plist"))->toArray();
